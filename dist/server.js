015006f97ce71594718aa09d8cbc7389195160a2
Alexandre Araujo <alexandre01393@gmail.com>
2022-05-22 13:18:40 -0300

    update: added dist folder

--- dist/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const http_1 = require("./http");
-const dotenv_1 = require("dotenv");
-require("./socket");
-(0, dotenv_1.config)();
-http_1.server.listen(process.env.PORT || 3333, () => {
-    console.log("Server running");
-});
