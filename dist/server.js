c887c777da908f0265c3939f7816289e3a8fe0b6
Alexandre Araujo <alexandre01393@gmail.com>
2022-05-22 13:14:16 -0300

    update: deploy

--- dist/server.js
+++ dist/server.js
@@ -4,6 +4,6 @@ const http_1 = require("./http");
 const dotenv_1 = require("dotenv");
 require("./socket");
 (0, dotenv_1.config)();
-http_1.server.listen(3333, () => {
+http_1.server.listen(process.env.PORT || 3333, () => {
     console.log("Server running");
 });
