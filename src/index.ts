9657308212531747800ead53da2dc409fbc4e2b7
Alexandre Araujo <alexandre01393@gmail.com>
2022-05-26 19:34:35 -0300

    fix: fixing bugs

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,17 @@
 import express from "express";
 import http from "http";
-import cors from 'cors'
+import cors from "cors";
 import { Server } from "socket.io";
 import { config } from "dotenv";
-
 
 config();
 
 const app = express();
-app.use(cors())
+app.use(cors());
 
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: { origin: "http://localhost:3000", methods: ["GET", "POST"] },
+  cors: { origin: process.env.ORIGIN, methods: ["GET", "POST"] },
 });
 
 interface RoomUser {
@@ -31,54 +30,52 @@ const users: RoomUser[] = [];
 const messages: Message[] = [];
 
 io.on("connection", (socket) => {
-    console.log(`User Connected: ${socket.id}`);
-  
-    socket.on("join_room", (data, callback) => {
-      socket.join(data);
+  console.log(`User Connected: ${socket.id}`);
 
-      const userInRoom = users.find(
-        ({ username, room }) => username === data.username && room === data.room
-      );
+  socket.on("join_room", (data, callback) => {
+    socket.join(data);
 
-      if (userInRoom) userInRoom.socketId = socket.id;
+    const userInRoom = users.find(
+      ({ username, room }) => username === data.username && room === data.room
+    );
 
-      users.push({
-        room: data.room,
-        username: data.username,
-        socketId: socket.id,
-      });
+    if (userInRoom) userInRoom.socketId = socket.id;
 
-      const messagesRoom = getMessagesRoom(data.room);
-      console.log(messagesRoom)
-      callback(messagesRoom);
+    users.push({
+      room: data.room,
+      username: data.username,
+      socketId: socket.id,
     });
-  
-    socket.on("send_message", (data) => {
 
-      const message: Message = {
-        room: data.room,
-        username: data.username,
-        message: data.message,
-        createdAt: new Date(),
-      };
-  
-      messages.push(message);
-      console.log(messages)
+    const messagesRoom = getMessagesRoom(data.room);
 
-      socket.to(data.room).emit("receive_message", message);
-    });
-  
-    socket.on("disconnect", () => {
-      console.log("User Disconnected", socket.id);  
-    });
-  });
-  
-  server.listen(3333, () => {
-    console.log("SERVER RUNNING");
+    callback(messagesRoom);
   });
 
-  const getMessagesRoom = (room: string) => {
-    const messagesRoom = messages.filter((message) => message.room === room);
-  
-    return messagesRoom;
-  };
+  socket.on("send_message", (data) => {
+    const message: Message = {
+      room: data.room,
+      username: data.username,
+      message: data.message,
+      createdAt: new Date(),
+    };
+
+    messages.push(message);
+
+    socket.to(data.room).emit("receive_message", message);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+
+server.listen(3333, () => {
+  console.log("SERVER RUNNING");
+});
+
+const getMessagesRoom = (room: string) => {
+  const messagesRoom = messages.filter((message) => message.room === room);
+
+  return messagesRoom;
+};
